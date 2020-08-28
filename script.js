0f0bbfe7bf6c738b5b8db8c41d7459c209065751
alex-badila <abadila123@gmail.com>
2020-08-28 14:52:04 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+var li = document.querySelector("li");
+var deleteButton = document.querySelector("delete");
+
+function inputLength() {
+	return input.value.length;
+}
+
+function createListElement() {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	input.value = "";
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+function strikeThrough(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("done");
+    }
+}
+
+function deleteItem(e) {
+    if (e.target.tagName === "LI") {
+        e.target.remove();
+    }
+}
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
+
+ul.addEventListener("click", strikeThrough);
+
+deleteButton.addEventListener("click", deleteItem);
